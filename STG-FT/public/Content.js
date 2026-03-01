8cf77c7f863626c64cf7215b54edab8bfc1b7cc6
scarryplays <anubhavdoc9654070617@gmail.com>
2026-03-02 01:39:37 +0530

    12 commit

--- STG-FT/public/Content.js
+++ STG-FT/public/Content.js
@@ -1,9 +1,39 @@
-console.log("STG is running");
-
 window.addEventListener("load", () => {
   setTimeout(() => {
-    if (window.STGloginDetection) window.STGloginDetection();
-    if (window.STGtrackerDetection) window.STGtrackerDetection();
-     if (window.STGtrustScore) window.STGtrustScore();
-  }, 1200);
+    const loginDetected = window.STGloginDetection();
+    const trackerCount = window.STGtrackerDetection();
+    const domain = window.location.hostname;
+    console.log("Sending to backend:", {
+  domain,
+  loginDetected,
+  trackerCount,
+});
+fetch("http://127.0.0.1:8000/api/trust/calculate/", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    domain,
+    loginDetected,
+    trackerCount,
+  }),
+})
+  .then((res) => {
+    console.log(" Raw response:", res);
+    console.log(" Status:", res.status);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(" Backend response:", data);
+
+    chrome.storage.local.set({
+      trustScore: data.trustScore,
+      reason: data.reason,
+    });
+  })
+  .catch((err) => {
+    console.error("Fetch error:", err);
+  });
+      }, 1000);
 });
