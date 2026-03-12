fe51728756011be08145ff29d7f781470130d291
scarryplays <anubhavdoc9654070617@gmail.com>
2026-03-12 22:43:05 +0530

    25 commit

--- trust/views.py
+++ trust/views.py
@@ -1,46 +1,76 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import DomainTrust
+import whois
+from datetime import datetime
+from .trusted_domain import KNOWN_SITE
+
+
+def get_domain_age(domain):
+    info = whois.whois(domain)
+    creation_date = info.creation_date
+
+    if isinstance(creation_date, list):
+        creation_date = creation_date[0]
+
+    age_days = (datetime.now() - creation_date).days
+    return age_days
 
 
 @api_view(["POST"])
 def calculate_trust(request):
+
     data = request.data
 
     domain = data.get("domain")
     login = data.get("loginDetected")
     trackers = data.get("trackerCount", 0)
 
-    score = "NEUTRAL"
-    reason = "No major signals"
-
-    if login and trackers > 5:
-        score = "RISK"
-        reason = "Login form with many trackers"
-    elif login and trackers > 2:
-        score = "CAUTION"
-        reason = "Login form detected"
-    elif login and trackers == 2:
-        score = "SAFE"
-        reason = "Login but no trackers"
-
-    DomainTrust.objects.update_or_create(
-        domain=domain,
-        defaults={
-            "trust_score":score,
-            "reason":reason
-        }
-    )
     existing = DomainTrust.objects.filter(domain=domain).first()
+
     if existing:
         return Response({
             "domain": existing.domain,
             "trustScore": existing.trust_score,
-            "reason": existing.reason   
+            "reason": existing.reason
         })
 
-    # return Response({
-    #     "domain": domain,
-    #     "trustScore": score,
-    #     "reason": reason
-    # })
+    score = "NEUTRAL"
+    reason = "No major signals"
+
+    if domain in KNOWN_SITE:
+        score = "SAFE"
+        reason = "Known trusted domain"
+
+    else:
+        age_days = get_domain_age(domain)
+
+        if age_days < 180:
+            score = "RISK"
+            reason = "New domain"
+
+        elif login and trackers > 5:
+            score = "RISK"
+            reason = "Login form with many trackers"
+
+        elif login and trackers > 2:
+            score = "CAUTION"
+            reason = "Login form detected"
+
+        elif login and trackers == 0:
+            score = "SAFE"
+            reason = "Login but no trackers"
+
+    DomainTrust.objects.update_or_create(
+        domain=domain,
+        defaults={
+            "trust_score": score,
+            "reason": reason
+        }
+    )
+
+    return Response({
+        "domain": domain,
+        "trustScore": score,
+        "reason": reason
+    })
