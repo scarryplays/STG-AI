1e1e2ab75d05fa51632fd2e460db600b4bdda352
scarryplays <anubhavdoc9654070617@gmail.com>
2026-03-14 21:11:56 +0530

    29 commit

--- trust/views.py
+++ trust/views.py
@@ -23,7 +23,7 @@ def get_domain_age(domain):
     except Exception as e:
         print(f"Error fetching WHOIS for {domain}: {e}")
         return 365
-
+# for com
 
 @api_view(["POST"])
 def calculate_trust(request):
