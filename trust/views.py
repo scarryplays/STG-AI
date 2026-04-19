6e9530b66c68a528b9c7471c1b8922d4c9868b45
scarryplays <anubhavdoc9654070617@gmail.com>
2026-04-19 21:46:05 +0530

    jbjhbvataas

--- trust/views.py
+++ trust/views.py
@@ -8,7 +8,6 @@ from .models import DomainTrust
 from .trusted_domain import KNOWN_SITE
 from ml.predictor import predict_url
 
-# knvslacacav
 def get_domain_age(domain):
     try:
         info = whois.whois(domain)
