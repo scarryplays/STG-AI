38bdbf51dd5f1549d3513ea283d00e1f6a79f472
scarryplays <anubhavdoc9654070617@gmail.com>
2026-04-30 22:06:26 +0530

    jbvataasaaaaaaasa

--- trust/views.py
+++ trust/views.py
@@ -35,7 +35,7 @@ def calculate_trust(request):
 
     if isinstance(url, bytes):
         url = url.decode("utf-8")
-        # FUCK THIS 
+        # FU
 
     if not isinstance(url, str):
         return Response({"error": "Invalid URL type"}, status=400)
